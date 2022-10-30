b449aacbb2f61fb18feed890c8e84876f2033e26
koreaamarin <awldnjs2@naver.com>
2022-10-30 23:21:25 +0900

    add tutorial part3 template incomplete1

--- 05.MJ/mysite/polls/views.py
+++ 05.MJ/mysite/polls/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request) :
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 1
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
 
-    
+    # 2
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list':latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # 1
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)      # 첫번째 인자는 모델을 받는다. 두번째 인자는 가져올 모델을 명시한다. 객체가 존재하지 않을 경우 404 예외를 발생시킨다.
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
