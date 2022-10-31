b5cd5cd722024846a01aee9247de0dd69a57dc56
DESKTOP-VA82SED\JIWON <awldnjs2@naver.com>
2022-11-01 04:16:05 +0900

    complate2 [django tutorial part6 # static file]

--- 05.MJ/mysite/polls/models.py
+++ 05.MJ/mysite/polls/models.py
@@ -12,7 +12,7 @@ class Question(models.Model) :
     def __str__(self):              # Question.objects.all()등의 API명령어를 통해 model을 호출할 때 모델안에 든 데이터를 출력하기 위한 함수.
         return self.question_text
 
-    def was_published_recently(self):
+    def was_published_recently(self):                                           # 생성일이 최근이면 True를, 최근이 아니면 False를 호출하는 함수.
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
--- 05.MJ/mysite/polls/urls.py
+++ 05.MJ/mysite/polls/urls.py
@@ -18,12 +18,19 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # 1
+    # # ex: /polls/
+    # path('', views.index, name='index'),
+    # # ex: /polls/5/
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # ex: /polls/5/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # # ex: /polls/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # 2
+    path('', views.IndexView.as_view(), name='index'),                          # listView는 지정해준 조건에 맞는 List들을 보여줄 때 자주 쓰임.
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),               # DetailView는 지정된 조건에 맞는 페이지의 상세내용을 보여주는 데 쓰임. 어차피 1개의 정보에 대해 상세내용을 보여줄거라서 pk를 url 구분 인자로 사용함. 
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
