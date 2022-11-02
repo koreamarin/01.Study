cfe04aa7f5aa7c35dceee0e72245f1c249de4525
DESKTOP-VA82SED\JIWON <awldnjs2@naver.com>
2022-11-03 02:48:58 +0900

    ongoing2 [jinstagram] utube2.21.33

--- 03.mychew/Jinstagram/Jinstargram/urls.py
+++ 03.mychew/Jinstagram/Jinstargram/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', views.)
+    path('main', views.Sub.as_view())
     
 ]
