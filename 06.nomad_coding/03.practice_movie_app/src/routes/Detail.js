4be98c411b5fb404126134bb7c7efecb2e34d95e
koreamarin <awldnjs2@naver.com>
2024-01-25 03:33:22 +0900

    react-msterclass

--- 06.nomad_coding/03.practice_movie_app/src/routes/Detail.js
+++ 06.nomad_coding/03.practice_movie_app/src/routes/Detail.js
@@ -1,16 +1,32 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
     const id = useParams().id
+    const [movie, setMovie] = useState(); // useState는 컴포넌트에서 동적인 데이터를 다룰 때 사용한다.
     const getMovie = async () => {
         const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
         console.log(json);
+        console.log(json.data.movie);
+        setMovie(json.data.movie);
     }
     useEffect(() => {
         getMovie();
     }, []);
-    return <h1>Detail</h1>
+    return (
+        <div>
+            <h1>Detail</h1>
+            <div>
+                {movie && (
+                    <div>
+                        <h1>{movie.title}</h1>
+                        <img src={movie.medium_cover_image}></img>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
+
 
 export default Detail;
